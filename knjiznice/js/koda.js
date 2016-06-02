18ff769f6836837d8f189b5284a156a3a86e8f7f
Jakob Bambič <jakob.bambic@gmail.com>
2016-06-02 01:55:19 +0000

    Končano dodajanje Google maps API

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -150,3 +150,54 @@ $(function () {
         }
     });
 });
+
+var map;
+var infowindow;
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14
+  });
+  
+  if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+            infowindow = new google.maps.InfoWindow();
+             var service = new google.maps.places.PlacesService(map);
+             service.nearbySearch({
+              location: pos,
+              radius: 1500,
+              type: ['hospital']
+                 }, callback);
+          });
+        }
+
+ 
+  
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
